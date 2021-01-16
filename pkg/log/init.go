605116ff83c712143831fc025be41e785d0eb3a7
zhengxirong <zh_season@163.com>
2021-01-16 16:55:56 +0800

    standard log format.

---
+++ pkg/log/init.go
@@ -0,0 +1,31 @@
+package log
+
+import (
+	"github.com/douyu/jupiter/pkg/xlog"
+	"go.uber.org/zap/zapcore"
+	"time"
+)
+
+func init() {
+	logConfig := xlog.Config{
+		Debug: true,
+		Async: true,
+		EncoderConfig: &zapcore.EncoderConfig{
+			TimeKey:       "ts",
+			LevelKey:      "lv",
+			NameKey:       "logger",
+			CallerKey:     "caller",
+			MessageKey:    "msg",
+			StacktraceKey: "stack",
+			LineEnding:    zapcore.DefaultLineEnding,
+			EncodeLevel:   zapcore.LowercaseLevelEncoder,
+			EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+				enc.AppendString(t.Format("2006-01-02 15:04:05"))
+			},
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+	}
+	xlog.DefaultLogger = logConfig.Build()
+	xlog.JupiterLogger = logConfig.Build()
+}
